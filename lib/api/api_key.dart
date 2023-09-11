2819a35739b49e61c50cbf0a15eba0dc26b2d9e6
Sourav Malani <74541281+Sourav-Malani@users.noreply.github.com>
2023-09-11 22:10:01 +0500

    Delete lib directory

--- lib/api/api_key.dart
+++
@@ -1,1 +0,0 @@
-const apiKey="a9049bab5d2e338aec23398ad63aaaad";
--- lib/utils/api_url.dart
+++
@@ -1,8 +0,0 @@
-import 'package:canker_detect/api/api_key.dart';
-
-String apiURL(var lat, var lon){
-  String url;
-  url=
-  "https://api.openweathermap.org/data/3.0/onecall?lat=$lat&lon=$lon&appid=$apiKey&units=metric&exclude=minutely";
-return url;
-}
